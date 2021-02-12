611606e304e926652165f5ec7d7c98667a1a9bb6
Simon Reuter <s.reuter1@stud.tu-darmstadt.de>
2021-02-12 11:16:40 +0100

    moved comment

--- threadsafe_queue.h
+++ threadsafe_queue.h
@@ -2,35 +2,6 @@
 #include <memory>
 #include <mutex>
 
-/* example usage:
-threadsafe_lut<int, std::string> lut;
-std::thread t0(&threadsafe_lut<int, std::string>::add_or_update_mapping, &lut, 6, "six");
-std::thread t1(&threadsafe_lut<int, std::string>::add_or_update_mapping, &lut, 7, "seven");
-std::thread t2(&threadsafe_lut<int, std::string>::add_or_update_mapping, &lut, 8, "eight");
-std::thread t3(&threadsafe_lut<int, std::string>::add_or_update_mapping, &lut, 9, "nine");
-
-t0.join();
-t1.join();
-t2.join();
-t3.join();
-
-std::string s;
-std::thread t4(&threadsafe_lut<int, std::string>::remove_mapping, &lut, 9);
-using overload_type = bool (threadsafe_lut<int, std::string>::*)(int, std::string&);
-overload_type value_for = &threadsafe_lut<int, std::string>::value_for;
-std::thread t5(value_for, &lut, 6, std::ref(s));
-
-t4.join();
-t5.join();
-
-std::cout << s << std::endl;
-
-std::thread t6(value_for, &lut, 9, std::ref(s));
-
-t6.join();
-std::cout << s << std::endl;
-*/
-
 template<typename T>
 class threadsafe_queue
 {
